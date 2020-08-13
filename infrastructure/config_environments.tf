e242ba2fa5f1aa0ab3106837e5858a286d24d3ad
Karol Piatkowski <k.piatkowski@codeandpepper.com>
2020-08-13 15:05:30 +0200

    Polishing

--- infrastructure/config_environments.tf
+++ infrastructure/config_environments.tf
@@ -5,15 +5,10 @@ locals {
     environments = {
         "dev" = {
             name = "development"
-            database = {
-                edition = "Basic"
-                max_size_gb = 2
-            }
             app_service = {
                 tier = "Basic"
                 size = "B2"
             }
-            domain  = "dev.werk8.app"
             container_settings = {
                 "NO_INDEX" = "1"
             }
@@ -22,15 +17,10 @@ locals {
         }
         "stg" = {
             name = "staging"
-            database = {
-                edition = "Basic"
-                max_size_gb = 2
-            }
             app_service = {
                 tier = "Basic"
                 size = "B2"
             }
-            domain  = "staging.werk8.app"
             container_settings = {
                 "NO_INDEX" = "1"
             }
@@ -39,15 +29,10 @@ locals {
         }
         "ephemeral" = {
             name = random_pet.preview.id
-            database = {
-                edition = "Basic"
-                max_size_gb = 2
-            }
             app_service = {
                 tier = "Basic"
                 size = "B2"
             }
-            domain = "${lower(local.application_name)}-${random_pet.preview.id}-app.azurewebsites.net"
             container_settings = {
                 "NO_INDEX" = "1"
             }
